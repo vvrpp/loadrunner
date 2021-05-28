63dca73f2be3523c64c4e840346c54dac1b707a2
unknown <VV Ramana@DESKTOP-JBR3CF7>
2021-05-28 09:27:05 +0530

    This is my first commit

---
+++ Action.c
@@ -0,0 +1,14 @@
+Action()
+{
+	
+	web_custom_request("web_custom_request",
+		"URL=www.example.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	return 0;
+}
